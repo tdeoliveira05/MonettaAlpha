843ac4a1e67225ab1c9882e1bf68d0507dafb675
Sunny Panchal <sunny@Sunnys-MacBook-Pro.local>
2018-01-20 01:34:38 -0500

    refactored and cleaned up code, and added proptypes

--- app/dumbComponents/DocumentStorage/DumbDocumentToolbar.js
+++ app/dumbComponents/DocumentStorage/DumbDocumentToolbar.js
@@ -1,81 +1,66 @@
-import React from 'react';
+import React        from 'react';
+import PropTypes    from 'prop-types';
 
 // Importing Material-UI Components
-import IconButton   from 'material-ui/IconButton';
-import IconMenu     from 'material-ui/IconMenu';
-import FlatButton   from 'material-ui/FlatButton';
-import MenuItem     from 'material-ui/MenuItem';
-import {Toolbar,
-        ToolbarGroup,
-        ToolbarSeparator,
-  ToolbarTitle}     from 'material-ui/Toolbar';
-import DropDownMenu from 'material-ui/DropDownMenu';
+import IconButton         from 'material-ui/IconButton';
+import IconMenu           from 'material-ui/IconMenu';
+import FlatButton         from 'material-ui/FlatButton';
+import RaisedButton       from 'material-ui/RaisedButton';
+import MenuItem           from 'material-ui/MenuItem';
+import {Toolbar}          from 'material-ui/Toolbar';
+import {ToolbarGroup}     from 'material-ui/Toolbar';
+import {ToolbarSeparator} from 'material-ui/Toolbar';
+import {ToolbarTitle}     from 'material-ui/Toolbar';
+import DropDownMenu       from 'material-ui/DropDownMenu';
 
 // Importing Material-UI SVG-Icons
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import MenuIcon     from 'material-ui/svg-icons/navigation/menu';
-import FilterListIcon from 'material-ui/svg-icons/content/filter-list';
-import SortIcon     from 'material-ui/svg-icons/content/sort';
+import MoreVertIcon       from 'material-ui/svg-icons/navigation/more-vert';
+import MenuIcon           from 'material-ui/svg-icons/navigation/menu';
+import FilterListIcon     from 'material-ui/svg-icons/content/filter-list';
+import SortIcon           from 'material-ui/svg-icons/content/sort';
 
 
 
 const DumbDocumentToolbar = (props) => {
-  //  props:
-  //    displayedMeetingFolder        (type: String)
-  //    onMeetingFolderDisplayChange  (type: Function)
+
+  let currentFolder         = props.currentFolder;
+  let onCurrentFolderChange = props.onCurrentFolderChange;
 
   return(
     <Toolbar className="DocumentStorageToolbar">
       <ToolbarGroup className="DSToolbarLeftGroup" firstChild={true}>
         <IconMenu
-          value={props.displayedMeetingFolder}
-          onChange={props.onMeetingFolderDisplayChange}
+          value={currentFolder}
+          onChange={onCurrentFolderChange}
           iconButtonElement={
             <IconButton>  <MenuIcon />  </IconButton>
           }
           targetOrigin={{horizontal: 'left', vertical: 'top'}}
           anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
         >
-          <MenuItem value='All Meetings'      primaryText="All Meetings"      />
-          <MenuItem value='Starred Meetings'  primaryText="Starred Meetings"  />
-          <MenuItem value='Recent Meetings'   primaryText="Recent Meetings"   />
-          <MenuItem value='Custom Category'   primaryText="Custom Category"   />
+          <MenuItem value='All Meetings'      primaryText="All Meetings"    />
+          <MenuItem value='Starred Meetings'  primaryText="Starred Meetings"/>
+          <MenuItem value='Recent Meetings'   primaryText="Recent Meetings" />
+          <MenuItem value='Custom Category'   primaryText="Custom Category" />
+          <MenuItem value='Trash'             primaryText='Trash'           />
         </IconMenu>
-        <ToolbarTitle text={props.displayedMeetingFolder}/>
+        <ToolbarTitle text={currentFolder}/>
       </ToolbarGroup>
       <ToolbarGroup className="DSToolbarRightGroup">
         <FlatButton
-          label="Filter"
-          labelPosition='after'
-          primary={false}
-          icon={<FilterListIcon />}
+          label="Create New Meeting"
+          primary={true}
         />
-        <FlatButton
-          label         = "Sort"
-          labelPosition = 'after'
-          primary       = {false}
-          icon          = {<SortIcon />}
-        />
-        <ToolbarSeparator />
-        {/*Additional Options Button (Download etc.)*/}
-        <IconMenu
-          iconButtonElement={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          targetOrigin={{horizontal: 'right', vertical: 'top'}}
-          anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-        >
-          <MenuItem   primaryText="Download All"      />
-          <MenuItem   primaryText="Download Selected" />
-          <MenuItem   primaryText="Share"             />
-          <MenuItem   primaryText="Settings"          />
-          <MenuItem   primaryText="Help & Feedback"   />
-        </IconMenu>
       </ToolbarGroup>
     </Toolbar>
   );
-}
+};
+
+//--------------------------EXPECTED PROP TYPES--------------------------------
+
+DumbDocumentToolbar.propTypes = {
+  currentFolder:          PropTypes.string.isRequired,
+  onCurrentFolderChange:  PropTypes.func.isRequired,
+};
 
 export default DumbDocumentToolbar
