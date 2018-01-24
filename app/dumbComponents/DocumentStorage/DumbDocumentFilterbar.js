89f4a995953bde6a3b9fe196cdf09b4b97147101
Sunny Panchal <sunny@Sunnys-MacBook-Pro.local>
2018-01-24 00:48:22 -0500

    filter menu now a popover, needs styling

--- app/dumbComponents/DocumentStorage/DumbDocumentFilterbar.js
+++ app/dumbComponents/DocumentStorage/DumbDocumentFilterbar.js
@@ -11,6 +11,11 @@ import Divider            from 'material-ui/Divider';
 import Paper              from 'material-ui/Paper';
 import SelectField        from 'material-ui/SelectField';
 import DatePicker         from 'material-ui/DatePicker';
+import DropDownMenu       from 'material-ui/DropDownMenu';
+import Popover            from 'material-ui/Popover';
+import Menu               from 'material-ui/Menu';
+
+
 
 
 // Importing Material-UI SVG-Icons
@@ -24,8 +29,14 @@ import ClearIcon          from 'material-ui/svg-icons/content/clear';
 const DumbDocumentFilterbar = (props) => {
 
   let onFilterButtonClick       = props.onFilterButtonClick;
-  let filterMenuStatus          = props.filterMenuStatus;
+  let showFilterMenu          = props.showFilterMenu;
   let filters                   = props.filters;
+  let filterMenuAnchor          = props.filterMenuAnchor;
+  let showSortMenu              = props.showSortMenu;
+  let sortBy                    = props.sortBy;
+  let onSortButtonClick         = props.onSortButtonClick;
+  let sortMenuAnchor            = props.sortMenuAnchor;
+  let onSortByChange            = props.onSortByChange;
   // filters.(location/participants/dateFrom/dateTo)
   let allLocations              = props.allLocations;
   let onLocationFilterChange    = props.onLocationFilterChange;
@@ -47,59 +58,122 @@ const DumbDocumentFilterbar = (props) => {
   }
 
   let filterMenu = () => {
-    if (filterMenuStatus === 'display') {
+    if (showFilterMenu) {
       return (
         <div className="FilterOptionsMenu" >
-          <Paper >
-            <SelectField
-              className="LocationFilterField"
-              floatingLabelText="Location"
-              value={filters.location}
-              onChange={onLocationFilterChange} >
-              <MenuItem value='Display All' primaryText='Display All' />
-              {allLocations.map((location, index) => {
-                return <MenuItem key={index} value={location} primaryText={location} />
-              })}
-            </SelectField >
-            <SelectField
-              className="ParticipantFilterField"
-              floatingLabelText="Participants"
-              multiple={true}
-              value={filters.participants}
-              onChange={onParticipantFilterChange}
-              selectionRenderer={selectionRenderer}>
-              <MenuItem value='Display All' primaryText='Display All' />
-              {allParticipants.map((participant, index) => {
-                return <MenuItem
-                  key={index + 1}
-                  insetChildren={true}
-                  checked={filters.participants.indexOf(participant) > -1}
-                  value={participant}
-                  primaryText={participant}
-                  />
-              })}
-            </SelectField >
-            <DatePicker
-              className="DateFromFilterField"
-              floatingLabelText="From Date"
-              container="inline"
-              value={filters.dateFrom}
-              onChange={onDateFromFilterChange}
-              autoOk={true}
-              maxDate={new Date()}/>
-            <DatePicker
-              className="DateToFilterField"
-              floatingLabelText="To Date"
-              container="inline"
-              value={filters.dateTo}
-              onChange={onDateToFilterChange}
-              autoOk={true}
-              maxDate={new Date()}/>
-            <IconButton className="ClearFilterButton">
-              <ClearIcon onClick={onClickClearFiltersButton}/>
-            </IconButton>
-          </Paper>
+          <Popover
+            open={showFilterMenu}
+            anchorEl={filterMenuAnchor}
+            anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            onRequestClose={onFilterButtonClick}>
+            <Menu>
+              <MenuItem>
+                <SelectField
+                  className="LocationFilterField"
+                  floatingLabelText="Location"
+                  fullWidth={true}
+                  value={filters.location}
+                  onChange={onLocationFilterChange} >
+                  <MenuItem value='Display All' primaryText='Display All' />
+                  {allLocations.map((location, index) => {
+                    return <MenuItem key={index} value={location} primaryText={location} />
+                  })}
+                </SelectField >
+              </MenuItem>
+              <MenuItem>
+                <SelectField
+                  className="ParticipantFilterField"
+                  floatingLabelText="Participants"
+                  fullWidth={true}
+                  multiple={true}
+                  value={filters.participants}
+                  onChange={onParticipantFilterChange}
+                  selectionRenderer={selectionRenderer}>
+                  <MenuItem value='Display All' primaryText='Display All' />
+                  {allParticipants.map((participant, index) => {
+                    return <MenuItem
+                      key={index + 1}
+                      insetChildren={true}
+                      checked={filters.participants.indexOf(participant) > -1}
+                      value={participant}
+                      primaryText={participant}
+                      />
+                  })}
+                </SelectField >
+              </MenuItem>
+              <MenuItem>
+                <DatePicker
+                  className="DateFromFilterField"
+                  floatingLabelText="From Date"
+                  container="inline"
+                  fullWidth={true}
+                  value={filters.dateFrom}
+                  onChange={onDateFromFilterChange}
+                  autoOk={true}
+                  maxDate={new Date()}/>
+              </MenuItem>
+              <MenuItem>
+                <DatePicker
+                  className="DateToFilterField"
+                  floatingLabelText="To Date"
+                  container="inline"
+                  fullWidth={true}
+                  value={filters.dateTo}
+                  onChange={onDateToFilterChange}
+                  autoOk={true}
+                  maxDate={new Date()}/>
+              </MenuItem>
+              <MenuItem>
+                <FlatButton
+                  label         ="Clear Filters"
+                  labelPosition ='before'
+                  onClick       ={onClickClearFiltersButton}
+                  primary       ={false}
+                  fullWidth     ={true}
+                  disableTouchRipple = {true}
+                  icon          ={<ClearIcon />}
+                />
+              </MenuItem>
+            </Menu>
+          </Popover>
         </div>
+      );
+    }
+  }
+
+  let sortMenu = () => {
+    if (showSortMenu) {
+      return(
+        <Popover
+          open={showSortMenu}
+          anchorEl={sortMenuAnchor}
+          anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'right', vertical: 'top'}}
+          onRequestClose={onSortButtonClick}>
+          <Menu
+            value={sortBy}
+            onChange={onSortByChange}>
+            <MenuItem
+              value='DateDesc'
+              primaryText='Date Descending'/>
+            <MenuItem
+              value='DateAsc'
+              primaryText='Date Ascending' />
+            <MenuItem
+              value='MeetingDesc'
+              primaryText='Meeting Name Descending'/>
+            <MenuItem
+              value='MeetingAsc'
+              primaryText='Meeting Name Ascending'/>
+            <MenuItem
+              value='LocDesc'
+              primaryText='Location Descending'/>
+            <MenuItem
+              value='LocAsc'
+              primaryText='Location Ascending' />
+          </Menu>
+        </Popover>
       );
     }
   }
@@ -122,13 +196,14 @@ const DumbDocumentFilterbar = (props) => {
         <FlatButton
           label         ="Filter"
           labelPosition ='after'
-          onClick       ={() => onFilterButtonClick()}
+          onClick       ={(event) => onFilterButtonClick(event)}
           primary       ={false}
           icon          ={<FilterListIcon />}
         />
         <FlatButton
           label         = "Sort"
           labelPosition = 'after'
+          onClick       = {(event) => onSortButtonClick(event)}
           primary       = {false}
           icon          = {<SortIcon />}
         />
@@ -145,13 +220,13 @@ const DumbDocumentFilterbar = (props) => {
         >
           <MenuItem   primaryText="Download Selected" />
           <MenuItem   primaryText="Delete Selected"   />
-          <MenuItem   primaryText="Share"             />
+          <MenuItem   primaryText="Share Selected"    />
           <MenuItem   primaryText="Settings"          />
           <MenuItem   primaryText="Help & Feedback"   />
         </IconMenu>
       </div>
-      {/*<Divider />*/}
       {filterMenu()}
+      {sortMenu()}
     </div>
   );
 }
@@ -159,15 +234,21 @@ const DumbDocumentFilterbar = (props) => {
 //-------------------------------PROP TYPES-------------------------------------
 DumbDocumentFilterbar.propTypes = {
   onFilterButtonClick:       PropTypes.func.isRequired,
-  filterMenuStatus:          PropTypes.string.isRequired,
+  showFilterMenu:            PropTypes.bool.isRequired,
   filters:                   PropTypes.object.isRequired,
+  showSortMenu:              PropTypes.bool.isRequired,
+  sortBy:                    PropTypes.string.isRequired,
+  sortMenuAnchor:            PropTypes.object,
+  filterMenuAnchor:          PropTypes.object,
+  onSortButtonClick:         PropTypes.func.isRequired,
+  onSortByChange:            PropTypes.func.isRequired,
   allLocations:              PropTypes.array.isRequired,
   onLocationFilterChange:    PropTypes.func.isRequired,
   allParticipants:           PropTypes.array.isRequired,
   onParticipantFilterChange: PropTypes.func.isRequired,
   onDateToFilterChange:      PropTypes.func.isRequired,
   onDateFromFilterChange:    PropTypes.func.isRequired,
-  onClickClearFiltersButton:         PropTypes.func.isRequired,
+  onClickClearFiltersButton: PropTypes.func.isRequired,
 };
 
 export default DumbDocumentFilterbar
