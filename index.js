d9d0b4d6ae80cae2387fbe0cedd82ba7bf1e643e
Thiago De Oliveira <tdeoliveira05@gmail.com>
2018-02-20 00:30:50 -0500

    ssl https redirect

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ try to access monetta.ai, Namecheap (our DNS) will redirect to https://monetta.a
 
 */
 
+// SSL Secure Certification redirect
+app.enable('trust proxy'); // enables X-Forwarded-Proto to be trusted
+app.use(function(req, res, next) {
+  if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+    res.redirect('https://' + req.get('Host') + req.url);
+  }
+  else
+    next();
+});
 
 //Middleware
 app.use(cors())
